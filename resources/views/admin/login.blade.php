6b9820933a6d0cdc5d7e8fa0d483a3eb5a8fada9
tusharkumar2302 <tusharkumar27864@gmail.com>
2024-09-11 19:47:57 +0530

    [setup]:asset,view,js,css files added

---
+++ resources/views/admin/login.blade.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<!-- Mirrored from adminlte.io/themes/v3/pages/examples/login-v2.html by HTTrack Website Copier/3.x [XR&CO'2014], Mon, 06 May 2024 05:17:02 GMT -->
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Admin Login</title>
+<base href="{{asset('admincss')}}/"/>
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&amp;display=fallback">
+
+<link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+
+<link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+
+<link rel="stylesheet" href="dist/css/adminlte.min2167.css?v=3.2.0">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+
+<div class="card card-outline card-primary">
+<div class="card-header text-center">
+<a href="../../index2.html" class="h1"><b>Login</b></a>
+</div>
+<div class="card-body">
+<p class="login-box-msg">Login to start your session</p>
+<form action="https://adminlte.io/themes/v3/index3.html" method="post">
+<div class="input-group mb-3">
+<input type="email" class="form-control" placeholder="Email">
+<div class="input-group-append">
+<div class="input-group-text">
+<span class="fas fa-envelope"></span>
+</div>
+</div>
+</div>
+<div class="input-group mb-3">
+<input type="password" class="form-control" placeholder="Password">
+<div class="input-group-append">
+<div class="input-group-text">
+<span class="fas fa-lock"></span>
+</div>
+</div>
+</div>
+<div class="row">
+<div class="col-8">
+<div class="icheck-primary">
+<input type="checkbox" id="remember">
+<label for="remember">
+Remember Me
+</label>
+</div>
+</div>
+
+<div class="col-4">
+<button type="submit" class="btn btn-primary btn-block">Login</button>
+</div>
+
+</div>
+</form>
+
+
+
+</div>
+
+</div>
+
+</div>
+
+
+<script src="plugins/jquery/jquery.min.js"></script>
+
+<script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+<script src="dist/js/adminlte.min2167.js?v=3.2.0"></script>
+</body>
+</html>
